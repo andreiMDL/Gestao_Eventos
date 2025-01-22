9697dd34a3cf62d526ff6ed37a8bc9dce098be43
l-fraga2811 <lucasbelter2805@gmail.com>
2025-01-22 20:12:51 -0300

    translated

--- gestao_eventos/src/main/java/br/com/gestao_eventos/gestao_eventos/modules/cliente/entities/ClientEntity.java
+++ gestao_eventos/src/main/java/br/com/gestao_eventos/gestao_eventos/modules/cliente/entities/ClientEntity.java
@@ -16,16 +16,15 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-@Entity(name = "cliente")
-public class ClienteEntity {
-    
-    @Id 
+@Entity(name = "client")
+public class ClientEntity {
+
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$",
-        message = "A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas, números e pelo menos um caracter especial.")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$", message = "A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas, números e pelo menos um caracter especial.")
     private String password;
 
     @CPF(message = "O campo deve conter um CPF válido")
@@ -35,7 +34,7 @@ public class ClienteEntity {
     private String email;
 
     @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "Número de telefone inválido")
-    private String telefone;
+    private String phone;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
--- gestao_eventos/src/main/java/br/com/gestao_eventos/gestao_eventos/modules/empresa/controllers/EventController.java
+++ gestao_eventos/src/main/java/br/com/gestao_eventos/gestao_eventos/modules/empresa/controllers/EventController.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.gestao_eventos.gestao_eventos.modules.empresa.entities.EventoEntity;
-import br.com.gestao_eventos.gestao_eventos.modules.empresa.useCases.CreateEventoUseCase;
+import br.com.gestao_eventos.gestao_eventos.modules.empresa.entities.EventEntity;
+import br.com.gestao_eventos.gestao_eventos.modules.empresa.useCases.CreateEventUseCase;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/evento")
-public class EventoController {
+@RequestMapping("/event")
+public class EventController {
 
       @Autowired
-      private CreateEventoUseCase createEventoUseCase;
+      private CreateEventUseCase createEventUseCase;
 
       @PostMapping("/")
-      public EventoEntity create(@Valid @RequestBody EventoEntity eventoEntity) {
-            return this.createEventoUseCase.execute(eventoEntity);
+      public EventEntity create(@Valid @RequestBody EventEntity eventEntity) {
+            return this.createEventUseCase.execute(eventEntity);
       }
 }
